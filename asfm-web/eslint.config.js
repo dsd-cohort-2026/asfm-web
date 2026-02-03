350cea50ec057a7c7c05eac2d6ca7e581d10b7b4
Michael Trew <40902786+CodeDiversity@users.noreply.github.com>
2026-02-03 09:03:35 -0500

    Update .gitignore, eslint.config.js, and package.json for improved linting and formatting

--- asfm-web/eslint.config.js
+++ asfm-web/eslint.config.js
@@ -1,8 +1,8 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import { defineConfig, globalIgnores } from 'eslint/config';
 
 export default defineConfig([
   globalIgnores(['dist']),
@@ -12,6 +12,9 @@ export default defineConfig([
       js.configs.recommended,
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
+      'eslint:recommended',
+      'plugin:react/recommended',
+      'plugin:prettier/recommended',
     ],
     languageOptions: {
       ecmaVersion: 2020,
@@ -24,6 +27,8 @@ export default defineConfig([
     },
     rules: {
       'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+      'prettier/prettier': 'error',
+      'react/react-in-jsx-scope': 'off',
     },
   },
-])
+]);
