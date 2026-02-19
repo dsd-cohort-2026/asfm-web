c6e9a550c52c54d28ebd10b17f034835d33bc2b6
kyle <nojaim.k@gmail.com>
2026-02-18 22:13:20 -0500

    verify jwt using jose, check user is staff

---
+++ server/middleware/auth-staff.js
@@ -0,0 +1,24 @@
+const { jwtVerify } = require('jose');
+const PROJECT_JWKS = require('../connections/jwks-key');
+
+module.exports = authMiddleware = async (req, res, next) => {
+  const token = req.headers.authorization?.split('Bearer ')[1];
+  if (!token) {
+    const err = new Error('No token found!');
+    err.statusCode = 401;
+    return next(err);
+  }
+
+  const { payload } = await jwtVerify(token, PROJECT_JWKS);
+
+  const user = await prisma.user.findUniqueOrThrow({
+    where: { id: payload.sub },
+  });
+
+  if (user.role !== 'STAFF') {
+    const err = new Error('You are not authorized to do this!');
+    err.statusCode = 401;
+    return next(err);
+  }
+  next();
+};
