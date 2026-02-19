108e859875ff968fabf690db06fc5eb4cb4620e1
kyle <nojaim.k@gmail.com>
2026-02-19 15:44:02 -0500

    imported prisma to auth checks, wrapped in try catch to add error status codes

--- server/middleware/auth-user.js
+++ server/middleware/auth-user.js
@@ -1,24 +1,30 @@
 const { jwtVerify } = require('jose');
 const PROJECT_JWKS = require('../connections/jwks-key');
+const prisma = require('../connections/prisma-client');
 
 module.exports = authMiddleware = async (req, res, next) => {
-  const token = req.headers.authorization?.split('Bearer ')[1];
-  if (!token) {
-    const err = new Error('No token found!');
+  try {
+    const token = req.headers.authorization?.split('Bearer ')[1];
+    if (!token) {
+      const err = new Error('No token found!');
+      err.statusCode = 401;
+      return next(err);
+    }
+
+    const { payload } = await jwtVerify(token, PROJECT_JWKS);
+
+    const user = await prisma.user.findUniqueOrThrow({
+      where: { id: payload.sub },
+    });
+
+    if (user.role !== 'STAFF' && user.role !== 'USER') {
+      const err = new Error('You must sign in!');
+      err.statusCode = 401;
+      return next(err);
+    }
+    next();
+  } catch (err) {
     err.statusCode = 401;
     return next(err);
   }
-
-  const { payload } = await jwtVerify(token, PROJECT_JWKS);
-
-  const user = await prisma.user.findUniqueOrThrow({
-    where: { id: payload.sub },
-  });
-
-  if (user.role !== 'STAFF' && user.role !== 'USER') {
-    const err = new Error('You must sign in!');
-    err.statusCode = 401;
-    return next(err);
-  }
-  next();
 };
