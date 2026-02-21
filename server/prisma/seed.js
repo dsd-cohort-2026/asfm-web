27aa1a633f38f4fcd20333ad35ed096ea0c18aa2
kyle <nojaim.k@gmail.com>
2026-02-21 16:34:56 -0500

    10 animals and users

--- server/prisma/seed.js
+++ server/prisma/seed.js
@@ -1,100 +1,430 @@
 const prisma = require('../../connections/prisma-client');
 
-const USERS = [
-  // 10 total users: 3 STAFF, 7 USER [1/7 implicit USER]
-  {
-    first_name: 'Bob',
-    last_name: 'Jenkinson',
-    email: 'b.jenkinson@gmail.com',
-    phone: '123-456-7890',
-    address: 'New York',
-    role: 'STAFF',
-  },
-  {
-    first_name: 'Midge',
-    last_name: 'Weston',
-    email: 'weston@gmail.com',
-    phone: '098-765-4321',
-    address: 'Wyoming',
-    role: 'STAFF',
-  },
-  {
-    first_name: 'Glenn',
-    last_name: 'Pritcher',
-    email: 'pritcher@hotmail.com',
-    phone: '555-555-5555',
-    address: 'California',
-    role: 'STAFF',
-  },
-  {
-    first_name: 'Sara',
-    last_name: 'Dunmore',
-    email: 's.dunmore@gmail.com',
-    phone: '312-847-9021',
-    address: 'Illinois',
-    role: 'USER',
-  },
-  {
-    first_name: 'Carlos',
-    last_name: 'Vega',
-    email: 'c.vega@yahoo.com',
-    phone: '713-204-5678',
-    address: 'Texas',
-  },
-  {
-    first_name: 'Priya',
-    last_name: 'Nair',
-    email: 'p.nair@gmail.com',
-    phone: '404-339-1122',
-    address: 'Georgia',
-    role: 'USER',
-  },
-  {
-    first_name: 'Tommy',
-    last_name: 'Halbert',
-    email: 'thalbert@outlook.com',
-    phone: '206-778-4490',
-    address: 'Washington',
-    role: 'USER',
-  },
-  {
-    first_name: 'Dana',
-    last_name: 'Osei',
-    email: 'd.osei@gmail.com',
-    phone: '617-552-8834',
-    address: 'Massachusetts',
-    role: 'USER',
-  },
-  {
-    first_name: 'Kenji',
-    last_name: 'Morrow',
-    email: 'k.morrow@hotmail.com',
-    phone: '503-491-7763',
-    address: 'Oregon',
-    role: 'USER',
-  },
-  {
-    first_name: 'Lena',
-    last_name: 'Hartwell',
-    email: 'lhartwell@gmail.com',
-    phone: '702-883-2215',
-    address: 'Nevada',
-    role: 'USER',
-  },
-];
+// await prisma.$transaction(async (prisma) => {
+//   await Promise.all([
+//     prisma.user.findMany(),
+//     prisma.user.findMany(),
+//     prisma.user.findMany(),
+//     prisma.user.findMany(),
+//     prisma.user.findMany(),
+//     prisma.user.findMany(),
+//     prisma.user.findMany(),
+//     prisma.user.findMany(),
+//     prisma.user.findMany(),
+//     prisma.user.findMany(),
+//   ]);
+// });
+
+// REMOVE AWAITS ON DEFINITION!!!
+// const write1 = prisma.user.create()
+// const write2 = prisma.post.create()
+// const write3 = prisma.profile.create()
+// await prisma.$transaction([write1, write2, write3])
+
+// const main = async () => {
+//   const awsProvider = await prisma.provider.create({
+//     data: { name: "AWS" },
+//   });
+//   const gcpProvider = await prisma.provider.create({
+//     data: { name: "GCP" },
+//   });
+
+//   await prisma.user.create({
+//     data: {
+//       email: "moderator@gmail.com",
+//       username: "timothy",
+//       role: "MODERATOR",
+//       authored: {
+//         create: [
+//           {
+//             name: "Simple Storage Service",
+//             acronym: "S3",
+//             provider: {
+//               connect: { id: awsProvider.id },
+//             },
+//           },
+//           {
+//             name: "DynamoDB",
+//             acronym: "DynamoDB",
+//             provider: {
+//               connect: { id: awsProvider.id },
+//             },
+//           },
+//           {
+//             name: "API Gateway",
+//             acronym: "API Gateway",
+//             provider: {
+//               connect: { id: awsProvider.id },
+//             },
+//           },
+//           {
+//             name: "BigQuery",
+//             acronym: "BigQuery",
+//             provider: {
+//               connect: { id: gcpProvider.id },
+//             },
+//           },
+//         ],
+//       },
+//     },
+//   });
+// };
 
 const seedTheBase = async () => {
   await prisma.user.deleteMany({});
-  const staffUser1 = await prisma.user.create(USERS[0]);
-  const staffUser2 = await prisma.user.create(USERS[1]);
-  const staffUser3 = await prisma.user.create(USERS[2]);
-  const user1 = await prisma.user.create(USERS[3]);
-  const user2 = await prisma.user.create(USERS[4]);
-  const user3 = await prisma.user.create(USERS[5]);
-  const user4 = await prisma.user.create(USERS[6]);
-  const user5 = await prisma.user.create(USERS[7]);
-  const user6 = await prisma.user.create(USERS[8]);
-  const user7 = await prisma.user.create(USERS[9]);
+  // 10 total users: users: 1-3 (STAFF), users: 4-10 (USER)
+  const user1 = await prisma.user.create({
+    data: {
+      first_name: 'Bob',
+      last_name: 'Jenkinson',
+      email: 'b.jenkinson@gmail.com',
+      phone: '123-456-7890',
+      address: 'New York',
+      role: 'STAFF',
+    },
+  });
+
+  const user2 = await prisma.user.create({
+    data: {
+      first_name: 'Midge',
+      last_name: 'Weston',
+      email: 'weston@gmail.com',
+      phone: '098-765-4321',
+      address: 'Wyoming',
+      role: 'STAFF',
+    },
+  });
+
+  const user3 = await prisma.user.create({
+    data: {
+      first_name: 'Glenn',
+      last_name: 'Pritcher',
+      email: 'pritcher@hotmail.com',
+      phone: '555-555-5555',
+      address: 'California',
+      role: 'STAFF',
+    },
+  });
+
+  const user4 = await prisma.user.create({
+    data: {
+      first_name: 'Sara',
+      last_name: 'Dunmore',
+      email: 's.dunmore@gmail.com',
+      phone: '312-847-9021',
+      address: 'Illinois',
+      role: 'USER',
+    },
+  });
+
+  // missing: defined role (default)
+  const user5 = await prisma.user.create({
+    data: {
+      first_name: 'Carlos',
+      last_name: 'Vega',
+      email: 'c.vega@yahoo.com',
+      phone: '713-204-5678',
+      address: 'Texas',
+    },
+  });
+
+  const user6 = await prisma.user.create({
+    data: {
+      first_name: 'Priya',
+      last_name: 'Nair',
+      email: 'p.nair@gmail.com',
+      phone: '404-339-1122',
+      address: 'Georgia',
+      role: 'USER',
+    },
+  });
+
+  const user7 = await prisma.user.create({
+    data: {
+      first_name: 'Tommy',
+      last_name: 'Halbert',
+      email: 'thalbert@outlook.com',
+      phone: '206-778-4490',
+      address: 'Washington',
+      role: 'USER',
+    },
+  });
+
+  const user8 = await prisma.user.create({
+    data: {
+      first_name: 'Dana',
+      last_name: 'Osei',
+      email: 'd.osei@gmail.com',
+      phone: '617-552-8834',
+      address: 'Massachusetts',
+      role: 'USER',
+    },
+  });
+
+  const user9 = await prisma.user.create({
+    data: {
+      first_name: 'Kenji',
+      last_name: 'Morrow',
+      email: 'k.morrow@hotmail.com',
+      phone: '503-491-7763',
+      address: 'Oregon',
+      role: 'USER',
+    },
+  });
+
+  const user10 = await prisma.user.create({
+    data: {
+      first_name: 'Lena',
+      last_name: 'Hartwell',
+      email: 'lhartwell@gmail.com',
+      phone: '702-883-2215',
+      address: 'Nevada',
+      role: 'USER',
+    },
+  });
+
+  const animal1 = await prisma.animal.create({
+    data: {
+      name: 'Drogo',
+      chip_id: 123456,
+      dob: new Date('November 9 2013'),
+      sex: 'MALE',
+      species: 'DOG',
+      foster_status: 'ADOPTED',
+      kennel_id: 2,
+      altered: true,
+      weight: 67.2,
+      last_modified: new Date(),
+      picture: 'happy-puppy.jpeg',
+      animal_assignments: {
+        create: {
+          start_date: new Date(),
+          end_date: new Date('March 31 2026'),
+          status: 'ACTIVE',
+          foster_user_id: user9.id,
+          assigned_by_staff_id: user1.id,
+        },
+      },
+      modified_by: { connectOrCreate: { staff_user_id: user1.id } },
+    },
+  });
+
+  const animal2 = await prisma.animal.create({
+    data: {
+      name: 'Penny',
+      chip_id: 654321,
+      dob: new Date('December 23 2015'),
+      sex: 'FEMALE',
+      species: 'DOG',
+      foster_status: 'ADOPTED',
+      kennel_id: 6,
+      altered: true,
+      weight: 48.9,
+      last_modified: new Date(),
+      picture: 'sorta-happy-puppy.jpeg',
+      animal_assignments: {
+        create: {
+          start_date: new Date(),
+          end_date: new Date('May 31 2031'),
+          status: 'ACTIVE',
+          foster_user_id: user9.id,
+          assigned_by_staff_id: user1.id,
+        },
+      },
+      modified_by: { connectOrCreate: { staff_user_id: user1.id } },
+    },
+  });
+
+  const animal3 = await prisma.animal.create({
+    data: {
+      name: 'Caramel',
+      chip_id: 98765,
+      dob: new Date('March 12 2020'),
+      sex: 'MALE',
+      species: 'CAT',
+      foster_status: 'FOSTERED',
+      kennel_id: 16,
+      altered: true,
+      weight: 18.6,
+      last_modified: new Date(),
+      picture: 'grumpy-cat.jpeg',
+      // missing: end_date (optional)
+      animal_assignments: {
+        create: {
+          start_date: new Date(),
+          status: 'ACTIVE',
+          foster_user_id: user4.id,
+          assigned_by_staff_id: user2.id,
+        },
+      },
+      modified_by: { connectOrCreate: { staff_user_id: user2.id } },
+    },
+  });
+
+  const animal4 = await prisma.animal.create({
+    data: {
+      name: 'George',
+      chip_id: 123987,
+      dob: new Date('September 19 2018'),
+      sex: 'MALE',
+      species: 'CAT',
+      foster_status: 'FOSTERED',
+      kennel_id: 100,
+      altered: true,
+      weight: 14.6,
+      last_modified: new Date(),
+      picture: 'grumpy-cat2.jpeg',
+      animal_assignments: {
+        create: {
+          start_date: new Date(),
+          end_date: new Date('October 21 2029'),
+          status: 'ACTIVE',
+          foster_user_id: user4.id,
+          assigned_by_staff_id: user2.id,
+        },
+      },
+      modified_by: { connectOrCreate: { staff_user_id: user2.id } },
+    },
+  });
+
+  const animal5 = await prisma.animal.create({
+    data: {
+      name: 'Tigger',
+      chip_id: 12987,
+      dob: new Date('January 19 2009'),
+      sex: 'MALE',
+      species: 'CAT',
+      foster_status: 'FOSTERED',
+      kennel_id: 10,
+      altered: false,
+      weight: 21.6,
+      last_modified: new Date(),
+      picture: 'unperturbed-cat.jpeg',
+      animal_assignments: {
+        create: {
+          start_date: new Date(),
+          end_date: new Date('February 21 2027'),
+          status: 'ACTIVE',
+          foster_user_id: user7.id,
+          assigned_by_staff_id: user3.id,
+        },
+      },
+      modified_by: { connectOrCreate: { staff_user_id: user2.id } },
+    },
+  });
+
+  const animal6 = await prisma.animal.create({
+    data: {
+      name: 'Winnie',
+      chip_id: 12387,
+      dob: new Date('May 6 2012'),
+      sex: 'FEMALE',
+      species: 'CAT',
+      foster_status: 'FOSTERED',
+      kennel_id: 20,
+      altered: false,
+      weight: 12.6,
+      last_modified: new Date(),
+      picture: 'lounging-cat.jpeg',
+      animal_assignments: {
+        create: {
+          start_date: new Date(),
+          end_date: new Date('December 1 2026'),
+          status: 'ACTIVE',
+          foster_user_id: user10.id,
+          assigned_by_staff_id: user1.id,
+        },
+      },
+      modified_by: { connectOrCreate: { staff_user_id: user3.id } },
+    },
+  });
+
+  const animal7 = await prisma.animal.create({
+    data: {
+      name: 'Belle',
+      chip_id: 12397,
+      dob: new Date('January 5 2016'),
+      sex: 'FEMALE',
+      species: 'DOG',
+      foster_status: 'FOSTERED',
+      kennel_id: 206,
+      altered: true,
+      weight: 65.3,
+      last_modified: new Date(),
+      picture: 'puffy-dog.jpeg',
+      animal_assignments: {
+        create: {
+          start_date: new Date(),
+          end_date: new Date('August 12 2028'),
+          status: 'ACTIVE',
+          foster_user_id: user5.id,
+          assigned_by_staff_id: user2.id,
+        },
+      },
+      modified_by: { connectOrCreate: { staff_user_id: user2.id } },
+    },
+  });
+
+  const animal8 = await prisma.animal.create({
+    data: {
+      name: 'Melky',
+      chip_id: 12398,
+      dob: new Date('November 12 2010'),
+      sex: 'MALE',
+      species: 'DOG',
+      foster_status: 'FOSTERED',
+      kennel_id: 8,
+      altered: true,
+      weight: 61.8,
+      last_modified: new Date(),
+      picture: 'bulky-dog.jpeg',
+      animal_assignments: {
+        create: {
+          start_date: new Date(),
+          end_date: new Date('September 2 2028'),
+          status: 'ACTIVE',
+          foster_user_id: user6.id,
+          assigned_by_staff_id: user2.id,
+        },
+      },
+      modified_by: { connectOrCreate: { staff_user_id: user3.id } },
+    },
+  });
+
+  // no animal-assignment for animal9 and animal10
+  const animal9 = await prisma.animal.create({
+    data: {
+      name: 'Bojack',
+      chip_id: 1238,
+      dob: new Date('March 22 2022'),
+      sex: 'MALE',
+      species: 'DOG',
+      foster_status: 'SHELETERED',
+      kennel_id: 45,
+      altered: true,
+      weight: 77.8,
+      last_modified: new Date(),
+      picture: 'big-dog.jpeg',
+      modified_by: { connectOrCreate: { staff_user_id: user1.id } },
+    },
+  });
+
+  const animal10 = await prisma.animal.create({
+    data: {
+      name: 'Sasha',
+      chip_id: 238,
+      dob: new Date('May 2 2024'),
+      sex: 'FEMALE',
+      species: 'DOG',
+      foster_status: 'SHELETERED',
+      kennel_id: 5,
+      altered: true,
+      weight: 57.5,
+      last_modified: new Date(),
+      picture: 'puffy-dog2.jpeg',
+      modified_by: { connectOrCreate: { staff_user_id: user1.id } },
+    },
+  });
 };
 
 const main = async () => {
@@ -109,54 +439,6 @@ const main = async () => {
 };
 
 main();
-
-const user1 = await prisma.user.create({
-  data: {
-    first_name: 'Bob',
-    last_name: 'Jenkinson',
-    email: 'b.jenkinson@gmail.com',
-    phone: '123-456-7890',
-    address: 'New York',
-    role: 'STAFF',
-  },
-});
-
-const user2 = await prisma.user.create({
-  data: {
-    first_name: 'Kenji',
-    last_name: 'Morrow',
-    email: 'k.morrow@hotmail.com',
-    phone: '503-491-7763',
-    address: 'Oregon',
-    role: 'USER',
-  },
-});
-
-const animal1 = await prisma.animal.create({
-  data: {
-    name: 'Drogo',
-    chip_id: 123456,
-    dob: new Date('November 9 2013'),
-    sex: 'MALE',
-    species: 'DOG',
-    foster_status: 'ADOPTED',
-    kennel_id: 2,
-    altered: true,
-    weight: 67.2,
-    last_modified: new Date(),
-    picture: 'happy-puppy.jpeg',
-    animal_assignments: {
-      create: {
-        start_date: new Date(),
-        end_date: new Date('March 31 2026'),
-        status: 'ACTIVE',
-        foster_user_id: user2.id,
-        assigned_by_staff_id: user1.id,
-      },
-    },
-    modified_by: { create: { staff_user_id: user1.id } },
-  },
-});
 
 const item1 = await prisma.item.create({
   data: {
