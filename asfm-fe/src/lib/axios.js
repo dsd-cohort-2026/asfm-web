c458306e2428ae43ad0862b135b63b94b34ea612
Ryan Griego <ryangriego@gmail.com>
2026-02-21 08:56:05 -0800

    Added json-server with mock data and postman json

--- asfm-fe/src/lib/axios.js
+++ asfm-fe/src/lib/axios.js
@@ -1,7 +1,12 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: 'http://localhost:8080/api',
+  // This points to real server API
+  // baseURL: 'http://localhost:8080/api',
+
+  // This points to mock API
+  baseURL: 'http://localhost:3001/api',
+
   timeout: 15000,
   headers: {
     'Content-Type': 'application/json',
