3beed785f9c18255b97d70ac5555340d97d303fc
Leon Zhao <lzhao92@gmail.com>
2026-02-23 01:56:04 -0500

    removed uneccessary classes from all buttons

--- asfm-fe/src/components/NonMemberSignInNavBar.jsx
+++ asfm-fe/src/components/NonMemberSignInNavBar.jsx
@@ -1,6 +1,6 @@
-import { Button } from "./ui/button";
-import { useAuthStore } from "../hooks/useAuthStore";
-import { useNavigate, useRouter } from '@tanstack/react-router'
+import { Button } from './ui/button';
+import { useAuthStore } from '../hooks/useAuthStore';
+import { useNavigate, useRouter } from '@tanstack/react-router';
 
 function TopNavBar() {
   const { isAuthenticated, login, logout } = useAuthStore();
@@ -9,35 +9,48 @@ function TopNavBar() {
     isAdmin: false, // Change this to true to test admin view
   };
   const navigate = useNavigate();
-  const router = useRouter()
-  
+  const router = useRouter();
+
   const handleAdminPortalClick = () => {
     navigate({ to: 'admin-portal' }); // Replace with your actual admin portal route
   };
   // Debug: Log all routes
-  console.log('Available routes:', router)
+  console.log('Available routes:', router);
   return (
     <nav className="bg-secondary p-4 flex justify-between">
       <div className="flex flex-grid items-center gap-4">
         {/* Create the logo */}
-         <div  className="rounded-md  text-secondary-foreground border-black px-4 py-2">Logo</div>
-        <Button variant="outline" className="rounded-full border border-secondary-foreground hover:bg-secondary-foreground hover:text-secondary">Home</Button>
+        <div className="rounded-md  text-secondary-foreground border-black px-4 py-2">Logo</div>
+        <Button
+          variant="outline"
+          className="rounded-full hover:bg-secondary-foreground hover:text-secondary"
+        >
+          Home
+        </Button>
         {/* is status it not guess add animal div */}
-        {isAuthenticated && user.isAdmin && <Button variant="outline" className="rounded-full border border-secondary-foreground hover:bg-secondary-foreground hover:text-secondary">Animal</Button>}
-        {isAuthenticated && !user.isAdmin && <Button variant="outline" className="rounded-full border border-secondary-foreground hover:bg-secondary-foreground hover:text-secondary" onClick={handleAdminPortalClick}>Admin Portal</Button>}
+        {isAuthenticated && user.isAdmin && <Button variant="outline">Animal</Button>}
+        {isAuthenticated && !user.isAdmin && (
+          <Button variant="outline" onClick={handleAdminPortalClick}>
+            Admin Portal
+          </Button>
+        )}
       </div>
       <div className="flex items-center gap-4">
         {!isAuthenticated ? (
           <>
-            <Button variant="outline" className="rounded-full border border-secondary-foreground hover:bg-secondary-foreground hover:text-secondary" onClick={login}>Sign In</Button>
-            <Button variant="outline" className="rounded-full border border-secondary-foreground hover:bg-secondary-foreground hover:text-secondary">Sign Up</Button>
+            <Button variant="outline" onClick={login}>
+              Sign In
+            </Button>
+            <Button variant="outline">Sign Up</Button>
           </>
         ) : (
           <div className="flex items-center gap-4">
             <div>
               <div className="bg-gray-200 px-4 py-2 rounded-2xl"> UserName</div>
             </div>
-            <Button variant="outline" className="rounded-full border text-secondary-foreground" onClick={logout}>Logout</Button>
+            <Button variant="outline" onClick={logout}>
+              Logout
+            </Button>
           </div>
         )}
       </div>
--- asfm-fe/src/components/basicNavBar.jsx
+++ asfm-fe/src/components/basicNavBar.jsx
@@ -1,41 +1,46 @@
-import { Button } from "./ui/button";
-import { useAuthStore } from "../hooks/useAuthStore";
-import { useNavigate, useRouter } from '@tanstack/react-router'
+import { Button } from './ui/button';
+import { useAuthStore } from '../hooks/useAuthStore';
+import { useNavigate, useRouter } from '@tanstack/react-router';
 
 function BasicNavBar() {
-const { isAuthenticated, login, logout } = useAuthStore();
+  const { isAuthenticated, login, logout } = useAuthStore();
   const navigate = useNavigate();
-  const router = useRouter()
-  
+  const router = useRouter();
+
   const handleLogOut = () => {
     logout();
     navigate({ to: '/' }); // Navigate to home page after logout
   };
   const handleMedicalLog = () => {
-    navigate({to: "/medical-logs"})
-  }
+    navigate({ to: '/medical-logs' });
+  };
 
   // Debug: Log all routes
   return (
     <nav className="bg-secondary p-4 flex justify-between">
       <div className="flex  items-center gap-4">
         {/* Create the logo */}
-         <div  className="rounded-md  text-secondary-foreground border-black px-4 py-2">Logo</div>
-        <Button variant="outline" className="rounded-full border border-secondary-foreground hover:bg-secondary-foreground hover:text-secondary">Home</Button>
+        <div className="rounded-md  text-secondary-foreground border-black px-4 py-2">Logo</div>
+        <Button variant="outline">Home1</Button>
         {/* is status it not guess add animal div */}
-        <Button variant="outline" className="rounded-full border border-secondary-foreground hover:bg-secondary-foreground hover:text-secondary">All Users</Button>
-        <Button variant="outline" className="rounded-full border border-secondary-foreground hover:bg-secondary-foreground hover:text-secondary text-s">My Animals</Button>
-        <Button variant="outline" className="rounded-full border border-secondary-foreground hover:bg-secondary-foreground hover:text-secondary">Inventory</Button>
-        <Button variant="outline" className="rounded-full border border-secondary-foreground hover:bg-secondary-foreground hover:text-secondary">Loans</Button>
-        <Button variant="outline" className="rounded-full border border-secondary-foreground hover:bg-secondary-foreground hover:text-secondary" onClick={handleMedicalLog}>Med Log</Button>
+        <Button variant="outline">All Users</Button>
+        <Button variant="outline" className="text-s">
+          My Animals
+        </Button>
+        <Button variant="outline">Inventory</Button>
+        <Button variant="outline">Loans</Button>
+        <Button variant="outline" onClick={handleMedicalLog}>
+          Med Log
+        </Button>
       </div>
       <div className="flex end gap-4">
-
         <div className="flex items-center gap-4">
-        <div>
+          <div>
             <div className="bg-gray-200 px-4 py-2 rounded-2xl"> UserName</div>
-        </div>
-        <Button variant="outline" className="rounded-full border border-secondary-foreground text-secondary-foreground hover:bg-secondary-foreground hover:text-secondary" onClick={handleLogOut}>Logout</Button>
+          </div>
+          <Button variant="outline" onClick={handleLogOut}>
+            Logout
+          </Button>
         </div>
       </div>
     </nav>
