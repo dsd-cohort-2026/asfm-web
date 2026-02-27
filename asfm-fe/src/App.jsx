5af99a452f4c940870e3fdaad2120c714b73530e
Ryan Griego <ryangriego@gmail.com>
2026-02-27 11:39:52 -0800

    ANIMALS-98-fe-layout-component - Added Layout component with footer and implemented it to existing pages

--- asfm-fe/src/App.jsx
+++ asfm-fe/src/App.jsx
@@ -1,16 +1,16 @@
 import FilterSelect from './components/custom/FilterSelect';
 import { Button } from './components/ui/button';
 import TopNavBar from './components/NonMemberSignInNavBar';
-import {ReusableTable} from './components/table_components'
-import {mockLoanedItems} from './features/mockLoanedItems'
+import { ReusableTable } from './components/table_components'
+import { mockLoanedItems } from './features/mockLoanedItems'
 import DashboardCard from './components/custom/DashboardCard'
 import { ModalDialog } from './components/ModalDialog';
 import ConfirmationDialog from './components/confirmationDialog';
 import { useState } from 'react';
 
 function App() {
- // src/features/loaned-items/loanedItemsColumns.js
- const loanedItemsColumns = [
+  // src/features/loaned-items/loanedItemsColumns.js
+  const loanedItemsColumns = [
     {
       accessorKey: "itemDescription",
       header: "Item Description",
@@ -96,14 +96,14 @@ function App() {
       </div>
       <div className='flex justify-center'>Admin Dashboard Card</div>
       <div className='grid grid-cols-1 md:grid-cols-2 gap-5 px-5'>
-        <DashboardCard title={"Testing Title"} navLink={"admin-portal"} itemsArray={[{name: 'Chewy', species: 'dog', sex: 'male', dob: '09/15/16'}, {name: 'Bailey', species: 'dog', sex: 'female', dob: '12/26/19'}]}/>
-        <DashboardCard title={"Testing Title 2"} navLink={"admin-portal"} itemsArray={[{name: 'Chewy', species: 'dog', sex: 'male', dob: '09/15/16'}, {name: 'Bailey', species: 'dog', sex: 'female', dob: '12/26/19'}]}/>
+        <DashboardCard title={"Testing Title"} navLink={"admin-portal"} itemsArray={[{ name: 'Chewy', species: 'dog', sex: 'male', dob: '09/15/16' }, { name: 'Bailey', species: 'dog', sex: 'female', dob: '12/26/19' }]} />
+        <DashboardCard title={"Testing Title 2"} navLink={"admin-portal"} itemsArray={[{ name: 'Chewy', species: 'dog', sex: 'male', dob: '09/15/16' }, { name: 'Bailey', species: 'dog', sex: 'female', dob: '12/26/19' }]} />
       </div>
-              <ReusableTable columns={loanedItemsColumns} data={mockLoanedItems}
+      <ReusableTable columns={loanedItemsColumns} data={mockLoanedItems}
         headerClassName="bg-secondary text-primary-foreground"
         tablebodyRowClassName="bg-white hover:bg-secondary/20"
         containerClassName='overflow-auto max-h-150 rounded-lg border border-pale-sky shadow-sm relative w-full px-4 lg:px-8'
-        />
+      />
     </>
   );
 }
