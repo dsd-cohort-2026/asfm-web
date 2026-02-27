0cd0bc05f56b9859b04fb6cd5bfdbca2fe90f780
Devan Rivera <devanrivera98@gmail.com>
2026-02-27 15:32:48 -0800

    ANIMALS-97 removed styling applied to the custom badge and updated isChecked conditional

--- asfm-fe/src/components/custom/CustomBadge.jsx
+++ asfm-fe/src/components/custom/CustomBadge.jsx
@@ -4,8 +4,8 @@ import { Badge } from '../ui/badge';
 
 export default function CustomBadge({ text, isChecked, badgeClassName, badgeCheckClassName }) {
   return (
-    <Badge className={cn('bg-highlight text-highlight-foreground p-0', badgeClassName)}>
-      {isChecked ? <BadgeCheck className={cn(badgeCheckClassName)} /> : <></>}
+    <Badge className={cn(badgeClassName)}>
+      {isChecked && <BadgeCheck className={cn(badgeCheckClassName)} />}
       <span>{text}</span>
     </Badge>
   );
