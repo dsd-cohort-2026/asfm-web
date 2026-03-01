8989665931f11f58daf4822a6a303e9f481921de
Leon Zhao <lzhao92@gmail.com>
2026-03-01 17:51:16 -0500

    added AuthProvider to main.jsx

--- asfm-fe/src/main.jsx
+++ asfm-fe/src/main.jsx
@@ -3,11 +3,15 @@ import { createRoot } from 'react-dom/client';
 import { RouterProvider, createRouter } from '@tanstack/react-router';
 import { routeTree } from './routeTree.gen'; // Auto-generated
 // Create the router instance
-const router = createRouter({ routeTree });
 import './index.css';
+import AuthProvider from './components/AuthProvider';
+
+const router = createRouter({ routeTree });
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
   </StrictMode>,
 );
