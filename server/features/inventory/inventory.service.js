400b89ae35a5a5ce81f6cd1a42792f514b0f214d
kyle <nojaim.k@gmail.com>
2026-03-05 00:14:08 -0500

    update separated object to conditionally add all props, throw on qty w/o transaction

--- server/features/inventory/inventory.service.js
+++ server/features/inventory/inventory.service.js
@@ -24,26 +24,36 @@ exports.getInventoryItemById = async (id) => {
 
 exports.updateInventory = async (req) => {
   try {
-    const inventory = {
-      id: req.params.id,
-      item_id: req.body.item_id,
-      quantity: req.body.quantity,
-    };
-    if (req.body.expiration_date) {
-      inventory.expiration_date = new Date(req.body.expiration_date);
+    if (req.body.quantity && !req.body.transaction_id) {
+      const err = new Error(
+        `You cannot update a quantity without also updating its corresponding transaction's quantity! Please provide a valid transaction ID!`,
+      );
+      err.statusCode = 400;
+      throw err;
     }
-    const inventory_transactions = {};
-    inventory_transactions.create = {
-      quantity: req.body.quantity,
-      status: req.body.status,
-      type: req.body.type,
-      notes: req.body.notes || '-',
-      staff_user: { connect: { id: req.body.staff_user } },
-      item: { connect: { id: req.body.item_id } },
-    };
-    if (req.body.foster_user) {
-      inventory_transactions.create.foster_user = { connect: { id: req.body.foster_user } };
+    const inventory = { id: req.params.id };
+    if (req.body.quantity || req.body.quantity === 0) inventory.quantity = req.body.quantity;
+    if (req.body.item_id) inventory.item_id = req.body.item_id;
+    if (req.body.expiration_date) inventory.expiration_date = new Date(req.body.expiration_date);
+
+    let inventory_transactions;
+    if (req.body.transaction_id) {
+      const update = {};
+      if (req.body.quantity || req.body.quantity === 0) update.quantity = req.body.quantity;
+      if (req.body.status) update.status = req.body.status;
+      if (req.body.type) update.type = req.body.type;
+      if (req.body.notes) update.notes = req.body.notes;
+      if (req.body.foster_user) update.foster_user = { connect: { id: req.body.foster_user } };
+      update.staff_user = { connect: { id: req.body.staff_user } };
+      if (req.body.item) update.item_id = { connect: { id: req.body.item_id } };
+      inventory_transactions = {
+        update: {
+          where: { id: req.body.transaction_id },
+          data: update,
+        },
+      };
     }
+
     const updatedInventory = await inventoryRepository.updateInventory({
       inventory,
       inventory_transactions,
